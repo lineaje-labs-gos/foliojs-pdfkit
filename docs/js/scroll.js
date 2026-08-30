b902eefb6e183ecadb8744f5877274f277a862e1
Luiz Americo <blikblum@users.noreply.github.com>
2026-08-30 20:36:10 -0300

    Update gh-pages branch

--- docs/js/scroll.js
+++ docs/js/scroll.js
@@ -1,38 +1,70 @@
-$(function() {
-  $('pre code').addClass('hljs coffeescript');
-  hljs.initHighlightingOnLoad();
-    
-  var sections = $("h2[id]");
-  window.onhashchange = function() {
-    $("a.selected").removeClass("selected");
-    $("a[href=" + location.hash + "]").addClass('selected');
+import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.2/es/core.min.js';
+import javascript from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.2/es/languages/javascript.min.js';
+
+hljs.registerLanguage('javascript', javascript);
+
+for (const codeBlock of document.querySelectorAll('pre code')) {
+  if (!codeBlock.className) {
+    codeBlock.classList.add('language-javascript');
   }
-  
-  $(window).bind("mousewheel DOMMouseScroll", function() {
-    var scrollTop = $(window).scrollTop() + document.documentElement.clientHeight / 3,
-        section = null,
-        link = null;
-        
-    for (var i = 0, len = sections.length; i < len; i++) {
-      var top = sections.eq(i).offset().top;
-          
-      if (i < len - 1) {
-        var nextTop = sections.eq(i + 1).offset().top;
-        if (scrollTop > top && scrollTop < nextTop) {
-          section = sections[i].id;
-          break;
-        }
-      }
+  hljs.highlightElement(codeBlock);
+}
+
+const sections = Array.from(document.querySelectorAll('h2[id]'));
+const navigationLinks = Array.from(
+  document.querySelectorAll('nav.sidebar a[href^="#"]'),
+);
+
+const selectSection = (section) => {
+  const hash = section ? `#${section.id}` : null;
+  for (const link of navigationLinks) {
+    link.classList.toggle('selected', link.getAttribute('href') === hash);
+  }
+};
+
+const selectHashTarget = () => {
+  let id;
+  try {
+    id = decodeURIComponent(location.hash.slice(1));
+  } catch {
+    return;
+  }
+  selectSection(sections.find((section) => section.id === id));
+};
+
+const selectScrolledSection = () => {
+  const viewportAnchor = window.innerHeight / 3;
+  let currentSection = null;
+
+  for (const section of sections) {
+    if (section.getBoundingClientRect().top > viewportAnchor) {
+      break;
     }
-    
-    if (!section) {
-      section = sections[len - 1].id;
+    currentSection = section;
+  }
+
+  selectSection(currentSection);
+};
+
+let scrolling = false;
+window.addEventListener(
+  'scroll',
+  () => {
+    if (scrolling) {
+      return;
     }
-    
-    link = $("a[href$=#" + section + "]");
-    if (!link.hasClass('selected')) {
-      $("a.selected").removeClass('selected');
-      link.addClass('selected');
-    }
-  });
-});
+    scrolling = true;
+    requestAnimationFrame(() => {
+      scrolling = false;
+      selectScrolledSection();
+    });
+  },
+  { passive: true },
+);
+window.addEventListener('hashchange', selectHashTarget);
+
+if (location.hash) {
+  selectHashTarget();
+} else {
+  selectScrolledSection();
+}
